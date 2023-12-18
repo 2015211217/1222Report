81d0ad34be0e53e38a15c1c94bf5148bfbdd52e7
JiangXuanke <15223370366@163.com>
2023-12-18 23:25:39 +0900

    12.18

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ extern crate rand;
 extern crate ndarray;
 extern crate ndarray_rand;
 extern crate plotters;
+extern crate gurobi;
 
 use ndarray_rand::{RandomExt, SamplingStrategy};
 use ndarray_rand::rand_distr::Uniform;
@@ -20,6 +21,7 @@ use plotters::prelude::*;
 use charming::{component::{Axis, Title}, element::AxisType, series::Line, Chart};
 use std::fs::{File, OpenOptions};
 use std::io::{Write, BufWriter, IoSlice};
+use gurobi::*;
 
 
 fn main() {
